48a17dfcfbc406b180e099cd00e94b45908b0a34
Evgeniy <gerardcobain1@gmail.com>
2023-10-23 13:54:46 +0300

    fix mobile menu close

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,7 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
-
+  const mobileLink = document.querySelectorAll(".mobile__link");
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
@@ -17,7 +17,12 @@
 
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
-
+  mobileLink.forEach((element) => {
+    element.addEventListener("click", () => {
+      mobileMenu.classList.remove("is-open");
+    });
+  });
+  console.log(mobileLink);
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;

fd30e22545bc46e3e7c705264d6fa8290e8842c8
Evgeniy <gerardcobain1@gmail.com>
2023-10-30 18:22:05 +0200

    update images

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -18,9 +18,7 @@
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
   mobileLink.forEach((element) => {
-    element.addEventListener("click", () => {
-      mobileMenu.classList.remove("is-open");
-    });
+    element.addEventListener("click", toggleMenu);
   });
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
